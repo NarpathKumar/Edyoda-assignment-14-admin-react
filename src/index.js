b22f411e8a3a5960f7a9929f26dbfec1fb387950
NarpathKumar <narpathkumar619@gmail.com>
2020-09-20 22:28:08 +0530

    sec commit

--- src/index.js
+++ src/index.js
@@ -3,10 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {Provider} from 'react-redux';
+import {createStore, combineReducers} from 'redux';
+import addproduct from './Containers/AddProduct/addproduct';
+import {mainReducer} from './Reducers/mainReducer';
+import {productReducer} from './Reducers/productsReducer';
+import {accountReducer} from './Reducers/accountReducer';
+
+const rootReducer = combineReducers({
+    wholeData: mainReducer,
+    productPageData: productReducer,
+    accountPageData: accountReducer
+})
+
+const globalStore = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={globalStore}>
+        <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
